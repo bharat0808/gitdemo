8ac0ae2a558479d3652ede2992d5e6f3251697a9
bharat0808 <bharatkhandare2@gmail.com>
2020-11-29 23:04:52 +0530

    Initial Version Commit

---
+++ calculatemodule.js
@@ -0,0 +1,12 @@
+exports.addition=function(a,b){
+	return parseInt(a)+parseInt(b);
+}
+exports.subtraction=function(a,b){
+	return parseInt(a)-parseInt(b);
+}
+exports.multiplication=function(a,b){
+	return parseInt(a)*parseInt(b);
+}
+exports.division=function(a,b){
+	return parseInt(a)/parseInt(b);
+}
